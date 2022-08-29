50c1f4ed97900453f709fb2f8f31e36e615b818d
Jung-Ah-C <wjddk718@gmail.com>
2022-08-29 15:48:15 +0900

    coding challenge #1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,59 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!')
+/*
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both 
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about 
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+m tall.
+GOOD LUCK
+*/
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+let markMass;
+let markHeight;
+let markBMI;
+
+let johnMass;
+let johnHeight;
+let johnBMI;
+
+let markHigherBMI;
+
+// case 1
+
+markMass = 78;
+markHeight = 1.69;
+
+johnMass = 92;
+johnHeight = 1.95;
+
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markBMI > johnBMI);
+
+// case 2
+
+markMass = 95;
+markHeight = 1.88;
+
+johnMass = 85;
+johnHeight = 1.76;
+
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markBMI > johnBMI);
